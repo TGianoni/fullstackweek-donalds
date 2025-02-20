38a1c1dc3a32597aba4a59ad491c4c688493b27a
TGianoni <gianoni2@hotmail.com>
2025-02-20 16:09:15 -0300

    feat: add initial cart context and cart sheet component

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -3,11 +3,14 @@
 import { Prisma } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format-currency";
+
+import { CartContext } from "../../contexts/cart";
+import CartSheet from "./cart-sheet";
 
 interface ProductDetailsProps {
     product: Prisma.ProductGetPayload<{include: {
@@ -21,6 +24,7 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({ product}: ProductDetailsProps) => {
+    const {toggleCart} = useContext(CartContext);
     const [quantity, setQuantity] = useState<number>(1)
     const handleDecreaseQuantity = () => {
         setQuantity((prev) => {
@@ -33,7 +37,12 @@ const ProductDetails = ({ product}: ProductDetailsProps) => {
     const handleIncreaseQuantity = () => {
         setQuantity((prev) => prev + 1)
     }
+
+    const handleAddToCart = () =>{
+        toggleCart()
+    }
     return ( 
+        <>
         <div className="relative z-50 rounded-t-3xl mt-[-1.5rem] p-5 flex-auto flex flex-col overflow-hidden">
                <div className="flex-auto overflow-hidden">
                  {/* RESTAURANTE */}
@@ -82,8 +91,11 @@ const ProductDetails = ({ product}: ProductDetailsProps) => {
                 </div>
                 </ScrollArea>
                </div>
-            <Button className="rounded-full w-full mt-6">Adicionar à sacola</Button>
+            <Button className="rounded-full w-full mt-6" onClick={handleAddToCart}>Adicionar à sacola</Button>
         </div>
+        <CartSheet />
+
+        </>
      );
 }
  
